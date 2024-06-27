b3eefc4fe858eafa49a3ae441a910890474f7bc2
teoxilu <teo.wasabu@gmail.com>
2024-06-28 00:51:18 +0700

    mongostring

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 //db
 mongoose
-  .connect(process.env.DATABASE_SERVER)
+  .connect('mongodb+srv://huyquang:huyquang123@huyecom.mlxzivo.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log("DB CONNECTED"))
   .catch((err) => console.log(`DB CONNECTION ERR`, err));
 
